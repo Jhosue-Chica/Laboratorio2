be89be1fa90407887e3e32296c0a271d061ba6f4
Jhosue-Chica <jichica@espe.edu.ec>
2024-07-18 13:00:30 -0500

    Añadir componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlantCardComponent } from './plant-card/plant-card.component';
+import { PlantDetailComponent } from './plant-detail/plant-detail.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PlantCardComponent,PlantDetailComponent,ShoppingCartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
