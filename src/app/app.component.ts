3913561e5997065442c97907bc835b81a931ec8b
nattypacheco17 <natalypacheco872@gmail.com>
2024-07-21 17:57:25 -0500

    Diseño de Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { PlantCardComponent } from './plant-card/plant-card.component';
 import { PlantDetailComponent } from './plant-detail/plant-detail.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,PlantCardComponent,PlantDetailComponent,ShoppingCartComponent,HeaderComponent],
+  imports: [RouterOutlet,PlantCardComponent,PlantDetailComponent,ShoppingCartComponent,HeaderComponent,FooterComponent,CarruselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
