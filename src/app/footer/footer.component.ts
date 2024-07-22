1ebf116220f5b63885a0eed5f97550c883f729ba
nattypacheco17 <natalypacheco872@gmail.com>
2024-07-21 21:23:33 -0500

    implementacion de listeners en footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
   @Input() title: string = '';
   @Input() frase: string = '';
+  @Input() showContactInfo: boolean = true;
+
+  onSocialMediaClick(platform: string) {
+    console.log(`${platform} icon clicked`);
+  }
+  
 }
