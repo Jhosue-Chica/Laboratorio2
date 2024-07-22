68ed9352784197fb3025f364e551bfdd70b67fa1
Jhosue-Chica <jichica@espe.edu.ec>
2024-07-21 23:59:04 -0500

    iindex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CarrouselComponent } from './carrousel/carrousel.component';
+import { PlantListComponent } from './plant-list/plant-list.component';
+
 
 @Component({
   selector: 'app-root',
@@ -18,7 +20,8 @@ import { CarrouselComponent } from './carrousel/carrousel.component';
     ShoppingCartComponent,
     HeaderComponent,
     FooterComponent,
-    CarrouselComponent
+    CarrouselComponent,
+    PlantListComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
